a3a3277979f8aee5a005b24ffdc0e605ebae1001
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-12-01 15:46:07 +0100

    #37 Create component mail

---
+++ system/email-component/src/main/java/fr/polytech/email/components/EmailBean.java
@@ -0,0 +1,57 @@
+package fr.polytech.email.components;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan("org.springframework.mail")
+public class EmailBean implements EmailSender {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Override
+    public void sendTemplateMessage(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+        helper.setFrom("noreply@polytech.unice.fr");
+        helper.setTo(to); 
+        helper.setSubject(subject); 
+        helper.setText(text, true);
+        emailSender.send(message);
+    }
+
+    @Override
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage(); 
+        message.setFrom("noreply@baeldung.com");
+        message.setTo(to); 
+        message.setSubject(subject); 
+        message.setText(text);
+        emailSender.send(message);
+    }
+
+    @Override
+    public void sendMultipleTemplateMessage(List<String> contacts, String subject, String text) throws MessagingException {
+        contacts.forEach(to -> {
+            try {
+                this.sendTemplateMessage(to, subject, text);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    
+}
